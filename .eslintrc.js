82b6bada0391513e1a645d1032143d6caed18243
Aakarshak Sthapak <akku_sthapak@live.com>
2025-02-16 17:13:10 +0530

    chore: init app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    '@react-native',
+    'plugin:oxlint/recommended',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        ignorePackages: true,
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/un-resolved': false,
+  },
 };
